83962084868ce2d45ef9b52313689a656b9c8bc1
Darunesh1 <daru22018.it@rmkec.ac.in>
2024-09-14 19:53:31 +0530

    improvements made to root and quick fnd algorithm

--- Algorithms_module_1/src/QuickUnion.java
+++ Algorithms_module_1/src/QuickUnion.java
@@ -1,19 +1,31 @@
 public class QuickUnion {
 
     private int[] id;
+    private int[] siz;
 
     public QuickUnion(int N)
     {
         for(int i=0;i<N;i++)
         {
             id[i]=i;
+            siz[i]=1;
         }
     }
+
+    // private int root(int i)
+    // {
+    //     while(i != id[i])
+    //     {
+    //         i=id[i];
+    //     }
+    //     return i;
+    // }
 
     private int root(int i)
     {
         while(i != id[i])
         {
+            id[i]=id[id[i]];
             i=id[i];
         }
         return i;
@@ -24,11 +36,30 @@ public class QuickUnion {
         return root(q)==root(p);
     }
 
+    // public void union(int p,int q)
+    // {
+    //     int i=root(p);
+    //     int j=root(q);
+    //     id[i]=j;
+    // }
+
     public void union(int p,int q)
     {
-        int i=root(p);
-        int j=root(q);
-        id[i]=j;
+        int i = root(p);
+        int j = root(q);
+        if(i==j)
+        {
+            return;
+        }
+        if(siz[i]<siz[j])
+        {
+            id[i]=j;
+            siz[j]+=siz[i];
+        }
+        else{
+            id[j]=i;
+            siz[i]+=siz[j];
+        }
     }
 
 
